941cf0a28ec0abea014525944c40d91b683f642f
Sujal Dave <sujaldave880@gmail.com>
2024-08-05 14:41:54 +0530

    Initial Commit

--- src/controllers/sendmailcontroller.js
+++ src/controllers/sendmailcontroller.js
@@ -1,26 +1,26 @@
-const nodemailer=require('nodemailer');
+const nodemailer = require('nodemailer');
 require('dotenv').config();
-const sendmailController=async(email,res)=>{
-    try{
-        const transporter=nodemailer.createTransport({
-            service:'gmail',
-            auth:{
-                user:process.env.USER_EMAIL,
-                pass:process.env.USER_PASS
+const sendmailController = async (email, res) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.USER_EMAIL,
+                pass: process.env.USER_PASS
             }
         });
-        const mailoptions={
-            from:process.env.USER_EMAIL,
-            to:email,
-            subject:"Welcome to Sujal's Spotify App"
+        const mailoptions = {
+            from: process.env.USER_EMAIL,
+            to: email,
+            subject: "Welcome to Sujal's Spotify App"
         }
         transporter.sendMail(mailoptions);
     }
-    catch(ex){
+    catch (ex) {
         res.status(500).json({
-            message:"internal server error"
+            message: "internal server error"
         })
     }
 }
 
-module.exports=sendmailController;
+module.exports = sendmailController;
